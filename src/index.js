b8c598efcc348522ddd3aab12bd86dd92185bf27
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-18 00:45:11 +0800

    kembali normal

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Helo Qim</h1>
+    
     <App></App>
   </React.StrictMode>
 );
