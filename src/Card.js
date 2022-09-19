ac14c1a2cf263a94e06b4cc298f8b1a28f22b15e
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-19 22:08:44 +0800

    Cards kena receive parameter, props

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src='https://robohash.org/mutaqim?200x200'/>
             <div>
-                <h2>Qim sem</h2>
-                <p>qim@getnada.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     );
