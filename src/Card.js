983bc01c1a07b1767079a4c0130141b26d4d28e2
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-19 21:55:05 +0800

    pa3  = besar sikit br3  =border rounded ma2 = pisahkan dengnan jarak grow = animation besarkan image kalau hover

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div className='bg-light-green dib'>
+        <div className='bg-light-green dib br3 pa3 ma2 grow'>
             <img alt='robots' src='https://robohash.org/mutaqim?200x200'/>
             <div>
                 <h2>Qim sem</h2>
