1f13b601eb16a6d801b6276417a4ba45c289b78b
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-19 22:09:01 +0800

    tc = text centre

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = (props) => {
     return (
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src='https://robohash.org/mutaqim?200x200'/>
             <div>
                 <h2>{props.name}</h2>
