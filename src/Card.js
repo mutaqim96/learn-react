6456f566f9f4fe1054ae775a2da42f1324aee55a
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-19 21:44:48 +0800

    simple component yang akan return Card

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='robots' src=''/>
+            <div>
+                <h2>Qim sem</h2>
+                <p>qim@getnada.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
