94a255cd7d8797b942834053aeb8b135abc1944d
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-19 21:47:14 +0800

    Adjust size. supaya make sure kecil

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
     return (
         <div>
-            <img alt='robots' src='https://robohash.org/mutaqim'/>
+            <img alt='robots' src='https://robohash.org/mutaqim?200x200'/>
             <div>
                 <h2>Qim sem</h2>
                 <p>qim@getnada.com</p>
