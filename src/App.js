4e1ff41972709afa8fd20d7a9a3fcc8fa22b119d
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-22 21:52:47 +0800

    interactive : One way data flow. antara SearchBox dengan CardList component.
    Sebelum ni pakai Props. Sekarang guna STATE
    
    kena guna class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>Robo Friends</h1>
-            <SearchBox/>
-            <CardList robots={robots}/>
-        </div>
-        
-    );
+const state = {
+    robots: robots,
+    searchfield: ''
 }
 
+class App extends Component {
+    constructor(){
+        
+    }
+
+    render(){
+        return (
+            <div className='tc'>
+                <h1>Robo Friends</h1>
+                <SearchBox/>
+                <CardList robots={robots}/>
+            </div>
+            
+        );
+    }
+    }
+    
+
 export default App;
+
