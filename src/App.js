7413609a0d4f1832152be3e4865f1bbc1d70e752
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-22 21:13:10 +0800

    SearchBox component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import {robots} from './robots';
 
 const App = () => {
     return (
         <div>
             <h1>Robo Friends</h1>
+            <SearchBox/>
             <CardList robots={robots}/>
         </div>
         
---
+++ src/SearchBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SearchBox() {
+  return (
+    <input type='search' placeholder='search robots' />
+  )
+}
+
+export default SearchBox
