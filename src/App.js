b7ab4a37e0e9977bb06a2677b54962fa3d957f47
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-22 22:02:41 +0800

    panggil function
    This. perlu ada sebab ni kan class

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
         return (
             <div className='tc'>
                 <h1>Robo Friends</h1>
-                <SearchBox/>
+                <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={this.state.robots}/>
             </div>
             
