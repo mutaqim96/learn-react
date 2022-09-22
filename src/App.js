84992db6a307783204898de09d2bbf94969ae4e2
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-22 21:10:44 +0800

    letak div, sekalikan dengan header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {robots} from './robots';
 const App = () => {
     return (
         <div>
+            <h1>Robo Friends</h1>
             <CardList robots={robots}/>
         </div>
         
