6e72364ceba5788657de6a91f358d323d7417dda
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-25 01:03:08 +0800

    this state kita buat property hasError yang ada initial value of false

--- src/ErrorBoundry.js
+++ src/ErrorBoundry.js
@@ -3,5 +3,8 @@ import React , {Component} from "react";
 class ErrorBoundry extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            hasError : false
+        }
     }
 }
