d67021377dd08ebb5bef02d22a64c84e296a77cc
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-25 00:46:29 +0800

    bagi je empty array. sebab searchfield akan berubah. tambah array kosong kita boleh replicate componentDidMount

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response =>  response.json())
         .then(users => setRobots(users))
-    }, [searchfield]);
+    }, []);
 
     const onSearchChange = (event) => {
         setSearchField(event.target.value);              
