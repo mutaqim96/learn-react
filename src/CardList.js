47142a92fd3d99785d087e968900d4415bcef073
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-25 01:12:59 +0800

    test

--- src/CardList.js
+++ src/CardList.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Card from './Card';
 
 function CardList({ robots }) {
+
+    if(true){
+        throw new Error('NOOOOO');
+    }
+
     const cardsArray = robots.map((user, i) => {
         return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
     } )
