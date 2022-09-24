58409562b1f266c85edaa0072f67a443521f6047
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-09-25 01:10:34 +0800

    import errorboundry

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState , useEffect } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import ErrorBoundry from './ErrorBoundry';
 
 import './App.css';
 
